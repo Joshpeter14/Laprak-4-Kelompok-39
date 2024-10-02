0462c29e84b2426cd8867135f9356013cc5d508f
Joshpeter14 <132868467+Joshpeter14@users.noreply.github.com>
2024-10-03 06:18:15 +0700

    Update Tugas.cpp

--- Tugas.cpp
+++ Tugas.cpp
@@ -50,3 +50,46 @@ void displaybukusStock(const std::vector<buku>& bukus) {
         }
     }
 }
+int main() {
+    std::vector<buku> bukus = {
+        buku("a book", 1),
+        buku("buku 2", 23),
+        buku("buku 3", 1337),
+        buku("buku 4", 42),
+        buku("buku 5", 0),
+        buku("buku 6", 888)
+    };
+
+    while (true) {
+        std::cout << "\nOpsi:" << std::endl;
+        std::cout << "1. Tambah Jenis buku" << std::endl;
+        std::cout << "2. Hapus Jenis buku" << std::endl;
+        std::cout << "3. Perbarui Stok buku" << std::endl;
+        std::cout << "4. Tampilkan Stok buku" << std::endl;
+        std::cout << "5. Keluar" << std::endl;
+
+        std::string option;
+        std::cout << "Masukkan Opsi (1/2/3/4/5): ";
+        std::getline(std::cin, option);
+
+        if (option == "1") {
+            while (true) {
+                std::string namabuku;
+                std::cout << "Masukkan Nama buku (ketik 'kembali' untuk ke menu): ";
+                std::getline(std::cin, namabuku);
+
+                if (namabuku == "kembali") break;
+
+                std::string stockStr;
+                std::cout << "Masukkan jumlah stock " << namabuku << " : ";
+                std::getline(std::cin, stockStr);
+
+                try {
+                    int stock = std::stoi(stockStr);
+                    bukus.emplace_back(namabuku, stock);
+                }
+                catch (const std::exception& e) {
+                    std::cout << "Input tidak valid. Silakan coba lagi." << std::endl;
+                }
+            }
+        }
